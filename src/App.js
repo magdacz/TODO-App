ae0f4d5bc86864faf424289880d5726b4c4c536f
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-05-09 22:11:07 +0200

    Add buttons

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import './App.css';
 
 
 class App extends Component {
+    
+    state = {
+        counter: 0,
+        tasks: []
+    }
+    
    render() {
        return (
            <div>
+            <h1>TODO App</h1>
+            <button>Add</button>
+            <button>Remove</button>
+            <button>Clear</button>
            </div>
        )
    } 
