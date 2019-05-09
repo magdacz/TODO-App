e0000122447dc0d6725b7f02fee9dae90bf2b574
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-05-09 22:30:02 +0200

    Add handleAddTask

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ class App extends Component {
         counter: 0,
         tasks: []
     }
+
+   handleAddTask = () => {
+       const tasks = [...this.state.tasks];
+       tasks.push(this.state.counter);
+       
+       this.setState({
+           counter: this.state.counter + 1,
+           tasks
+       })
+   }
     
    render() {
+       
+       const task = this.state.tasks.map((task, index) => <li key={index}> Task number {index+1} </li>)
+       
        return (
            <div>
             <h1>TODO App</h1>
-            <button>Add</button>
+            <button onClick={this.handleAddTask}>Add</button>
             <button>Remove</button>
             <button>Clear</button>
+            {this.state.tasks && <ul>{task}</ul>}
            </div>
        )
    } 
