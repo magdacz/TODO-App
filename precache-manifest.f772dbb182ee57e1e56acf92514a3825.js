b860e570589fb54a2183cb57a6890b4511095085
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-05-10 14:49:39 +0200

    Updates

---
+++ precache-manifest.f772dbb182ee57e1e56acf92514a3825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956f06538341a696878bf9465bbf52a8",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "cecd7d739f2e51723cb5",
+    "url": "/todo-list/static/css/main.304637ad.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/todo-list/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "cecd7d739f2e51723cb5",
+    "url": "/todo-list/static/js/main.29f969e6.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
